93cd96b68d6af859fd992debad7b2ef30a1a6560
RonNofar <ronnofar2@gmail.com>
2017-03-25 14:40:37 -0700

    Switch to NewtonVR
    In the process of the switch to NewtonVR, computer is having a lot of problems so I'm uploading unfinished code so it doesn't get lost.
    More to come during meeting.

---
+++ ProjectOlympus/Assets/Scripts/LevelSelectorHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Olympus.Showroom.UI
+{
+    public class LevelSelectorHandler : MonoBehaviour
+    {
+        [Header("Buttons")]
+        [SerializeField]
+        private Button artButton;
+        [SerializeField]
+        private Button musicButton;
+        [SerializeField]
+        private Button AssetsButton;
+
+
+        private void Awake()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
