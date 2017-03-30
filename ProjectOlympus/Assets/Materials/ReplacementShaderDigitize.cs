92a2254e012dbba92d154c21e658f1642a03bf66
EthanChing <forfer@msn.com>
2017-03-29 19:35:45 -0700

    Digitize Shader
    Example Solution to shader effect

---
+++ ProjectOlympus/Assets/Materials/ReplacementShaderDigitize.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ReplacementShaderDigitize : MonoBehaviour {
+
+    public Shader ReplacementShader;
+
+    void OnEnable()
+    {
+        if (ReplacementShader != null)
+            GetComponent<Camera>().SetReplacementShader(ReplacementShader, "RenderType");
+
+    }
+
+    void OnDisable()
+    {
+        GetComponent<Camera>().ResetReplacementShader();
+    }
+
+
+}
